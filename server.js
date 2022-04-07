9f9e5571ef70f4d7e566add886d274d9cadb5e5d
Dvagoes <43369743+Dvagoes@users.noreply.github.com>
2022-04-07 21:06:34 +0100

    proof of concept for home page posts with ejs

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-var express = require('express');
-var app = express();
-var url = require('url');
-
+const express = require('express');
+const app = express();
+const url = require('url');
+const ejs = require('ejs');
+const fs = require('fs')
+require('dotenv').config();
+const get_img = require('./modules/get_img');
+const path = require('path');
+const router = express.Router();
+const http = require('http');
+const bodyParser = require('body-parser');
+const route = require('./modules/route.js')
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('./public'));
 
+const mClient = require('mongodb').MongoClient;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dvagoes:<password>@cm4025.zjs6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const mClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-mClient.connect(url, function (err, client) {
+mClient.connect(process.env.uri, function (err, client) {
     if (err) throw err;
     db = client.db('CM4025');
     app.listen(8080);
@@ -18,7 +25,7 @@ mClient.connect(url, function (err, client) {
 });
 
 app.get('/', function (req, res) {
-    res.render('assets/pages/index.ejs');
+    res.render('index.ejs', {posts:posts});
 });
 
 app.get("/getUserData", function (req, res) {
@@ -27,9 +34,26 @@ app.get("/getUserData", function (req, res) {
         res.send(JSON.stringify(result));
     });
 });
+
 app.get("/getPosts", function (req, res) {
     db.collection('Posts').find(req.body).toArray(function (err, result) {
         if (err) throw err;
         res.send(JSON.stringify(result));
     });
 });
+
+var posts = 
+[
+    {
+        post_title: 'Test0',
+        img: './images/default.png',
+        score: 0,
+        datetime: '5/10/2022'
+    },
+    {
+        post_title: 'Test1',
+        img: './images/default.png',
+        score: 3,
+        datetime: '5/10/2022'
+    }
+];
